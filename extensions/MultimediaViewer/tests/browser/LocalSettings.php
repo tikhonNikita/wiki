5517a5e29790f376b072efd0a6fd1f7b0d174b2c
Nikita Tikhonov <tikhonnikita987@gmail.com>
2018-11-27 20:29:30 +0300

    adding extension

--- extensions/MultimediaViewer/tests/browser/LocalSettings.php
+++ extensions/MultimediaViewer/tests/browser/LocalSettings.php
@@ -1,4 +1,3 @@
 <?php
 	$wgUseInstantCommons = true;
     require_once "$IP/extensions/ConfirmAccount/ConfirmAccount.php";
-    $wgUpgradeKey = "B3438A89D9E4A3C50CCA6AF99B244A30BB41BBD1ADF788F029673E48A8E9DA42";
